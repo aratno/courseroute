6491430a3e8112c11390b9879e817adf9a44d01d
aratno <aratno@gmail.com>
2016-12-01 20:36:54 -0500

    Initial commit of data fetching files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # courseroute
+
+courseroute originated at Victoria College at the University of Toronto. The
+project hopes to serve as a more usable alternative to the course calendar
+through better visualization and interactivity.
+
+This project is using Python 3.4.1, with virtual environments to keep things
+organized.
---
+++ grab_data.py
@@ -0,0 +1,31 @@
+'''
+grab_data:
+    This file offers some basic functions for downloading local copies of
+    course data. This is to keep testing reliable and prevent bloat in the
+    repository. All data files are downloaded from the iit.utoronto.ca API
+    endpoint to a local data/ directory that is ignored by git.
+    Do not commit these files! They will only slow things down.
+
+author: Abe Ratnofsky
+'''
+
+import requests
+import time
+from flask import Flask
+
+'''
+This grabs a small(er) dataset; only 2nd-year CSC courses. It does not do as
+much error checking as it could, but this is a toy example.     ¯\_(ツ)_/¯
+'''
+def get_abridged_data():
+    url = 'https://timetable.iit.artsci.utoronto.ca/api/courses'
+    params = {'code':'CSC', 'section':'F', 'studyyear':2}
+    print('Fetching url...')
+    r = requests.get(url, params=params)
+    output_dir = 'data/'
+    f = open(output_dir + 'abridged_data_dump - '  + str(time.time()) + '.json', 'w') 
+    f.write(r.text)
+    print('File successfully written to ' + output_dir)
+
+if __name__ == '__main__':
+    get_abridged_data()
